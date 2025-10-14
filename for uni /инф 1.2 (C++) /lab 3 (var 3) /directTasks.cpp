02e43d35aaaa67c6650a849c650ede3db97c83e6
حُب <159039354+cxurescry@users.noreply.github.com>
2025-10-14 17:26:57 +0700

    -

---
+++ for uni /инф 1.2 (C++) /lab 3 (var 3) /directTasks.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+using namespace std;
+
+string toBase(unsigned int x, unsigned int base) {
+    string digits = "0123456789ABCDEFGHIJK";
+    string result;
+    while (x) {
+        result += digits[x % base];
+        x /= base;
+    }
+    reverse(begin(result), end(result));
+    return result;
+}
+
+// Преобразует символ в число
+int charToDigit(char c) {
+    if (isdigit(c))
+        return c - '0';
+    return toupper(c) - 'A' + 10;
+}
+
+// Преобразует число в символ
+char digitToChar(int d) {
+    if (d < 10)
+        return '0' + d;
+    return 'A' + (d - 10);
+}
+
+void task1() {
+    cout << "Введите основание системы счисления p (2 <= p <= 16): ";
+    unsigned int p; cin >> p;
+    if (p < 2 || p > 16) cout << "Вы неверно ввели основание СС.";
+
+    cout << "Введите положительное число N: ";
+    unsigned int n; cin >> n;
+    cout << endl;
+
+    cout << toBase(n, p) << endl;
+}
+
+void task2() {
+    cout << "Введите основание системы счисления p (2 <= p <= 16): ";
+    unsigned int p; cin >> p;
+    if (p < 2 || p > 16) cout << "Вы неверно ввели основание СС.";
+
+    string num; cin >> num;
+
+    int result;
+
+    for (char c : num) {
+        int digit;
+        if (isdigit(c)) {
+            digit = c - '0';
+        }
+        else {
+            digit = toupper(c) - 'A' + 10;
+        }
+        result = result * p + digit;
+    }
+    cout << num << "(" << p << ") = " << result << "(10)" << endl;
+}
+
+void task3() {
+    cout << "Введите основание системы счисления p (2 <= p <= 16): ";
+    unsigned int p; cin >> p;
+    if (p < 2 || p > 16) cout << "Вы неверно ввели основание СС.";
+
+    unsigned int m, n; cin >> m >> n;
+    string digits = "0123456789ABCDEFGHIJK";
+    string res1, res2;
+
+    res1 = toBase(m, p);
+    res2 = toBase(n, p);
+
+    // Выравниваем длины
+    while (res1.size() < res2.size()) res1 = "0" + res1;
+    while (res2.size() < res1.size()) res2 = "0" + res2;
+
+    string result = "";
+    int carry = 0;
+
+    cout << "\nПошаговое сложение в столбик:\n";
+
+    // Сложение справа налево
+    for (int i = res1.size() - 1; i >= 0; i--) {
+        int da = charToDigit(res1[i]);
+        int db = charToDigit(res2[i]);
+
+        int sum = da + db + carry;
+        int digit = sum % p;
+        carry = sum / p;
+
+        cout << res1[i] << " + " << res2[i]
+             << " + перенос " << carry
+             << " => цифра " << digitToChar(digit)
+             << " (перенос " << carry << ")\n";
+
+        result = digitToChar(digit) + result;
+    }
+
+    if (carry > 0)
+        result = digitToChar(carry) + result;
+
+    cout << "\nРезультат: " << res1 << "(" << p << ") + " << res2
+         << "(" << p << ") = " << result << "(" << p << ")\n";
+
+}
+
+int main() {
+    task1();
+    task2();
+    task3();
+    return 0;
+}
