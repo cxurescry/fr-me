9c319d469be2f0157db0daa8d081bc58128118b3
حُب <159039354+cxurescry@users.noreply.github.com>
2025-10-14 15:46:19 +0700

    -

--- for uni /ОПП (Python) /lab2 /main.py
+++ for uni /ОПП (Python) /lab2 /main.py
@@ -2,14 +2,15 @@ def replace_colums(xs):
     rows = len(xs)
     cols = len(xs[0])
 
-    last_pos = None
+    last_pos = None # создаем переменную для тех индексов, где в последний раз увиделм строку и столбец
     for i in range(rows):
         for j in range(cols):
             if xs[i][j] > 100:
                 last_pos = (i, j)
 
-    target_col = last_pos[1]
+    target_col = last_pos[1] # запоминаем столбец
 
+    # в цикле для каждой строки 2 и наш столбец который щапомнили мкеякм местами
     for i in range(rows):
         xs[i][1], xs[i][target_col] = xs[i][target_col], xs[i][1]
 
@@ -18,7 +19,7 @@ def replace_colums(xs):
 
 xs = [
     [12,  150,  3],
-    [ 8,   95, 102],
+    [8,   95, 102],
     [11,   70,  99]
 ]
 for row in xs: print(*row)
