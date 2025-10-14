f8253ffd29f3852090980b74d5c79cdc4336a78a
حُب <159039354+cxurescry@users.noreply.github.com>
2025-10-14 14:51:50 +0700

    Create mai.py

---
+++ for uni /ОПП (Python) /lab2 /mai.py
@@ -0,0 +1,27 @@
+def replace_colums(xs):
+    rows = len(xs)
+    cols = len(xs[0])
+
+    last_pos = None
+    for i in range(rows):
+        for j in range(cols):
+            if xs[i][j] > 100:
+                last_pos = (i, j)
+
+    target_col = last_pos[1]
+
+    for i in range(rows):
+        xs[i][1], xs[i][target_col] = xs[i][target_col], xs[i][1]
+
+    return xs
+
+
+xs = [
+    [12,  150,  3],
+    [8,   95, 102],
+    [11,   70,  99]
+]
+for row in xs: print(*row)
+replace_colums(xs)
+print()
+for row in xs: print(*row)
