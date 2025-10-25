5ae1968f9c7783e11a781dcbf9c7ed1fd86305d7
حُب <159039354+cxurescry@users.noreply.github.com>
2025-10-25 14:32:21 +0700

    4

---
+++ for uni /инф 1.2 (C++) /lab 5 /04.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+std::string toBinary(unsigned int num) {
+    std::string bin = "";
+    for (int i = 15; i >= 0; --i) {
+        bin += ((num >> i) & 1) ? '1' : '0';
+        if (i % 4 == 0 && i != 0) bin += ' ';
+    }
+    return bin;
+}
+
+int main() {
+    int X;
+    std::cout << "Введите число ";
+    std::cin >> X;
+
+    unsigned int X_unsigned = static_cast<unsigned int>(X);
+    unsigned int X_plus_1_unsigned = X_unsigned + 1;
+
+    std::cout << "       |  дес  |  восм  |  hex  |  двоичный" << std::endl;
+    std::cout << "x =    | " << std::setw(5) << X_unsigned << " | " << std::setw(6) << std::oct << X_unsigned << " | " << std::setw(4) << std::hex << X_unsigned << " | " << toBinary(X_unsigned) << std::endl;
+    std::cout << "x + 1 =| " << std::setw(5) << X_plus_1_unsigned << " | " << std::setw(6) << std::oct << X_plus_1_unsigned << " | " << std::setw(4) << std::hex << X_plus_1_unsigned << " | " << toBinary(X_plus_1_unsigned) << std::endl;
+
+    return 0;
+}
