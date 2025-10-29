83434409175c7e6b5b2ca309a3a6aae99151ecc5
حُب <159039354+cxurescry@users.noreply.github.com>
2025-10-29 11:26:13 +0700

    -

--- for uni /инф 1.2 (C++) /lab 2 (var 3) /task2.cpp
+++ for uni /инф 1.2 (C++) /lab 2 (var 3) /task2.cpp
@@ -3,15 +3,16 @@
 using namespace std;
 
 int main() {
+  cout << "Введите X и Y: ";
   double x, y; cin >> x >> y;
   if (x > 0 && x + y > 3) {
-    cout << log(sqrt(x + y) - 1);
+    cout << log(sqrt(x + y) - 1) << endl;
   }
   else if (x + y < 0) {
-    cout << (x - y + log(-1 / (x + y))) / (x + y);
+    cout << (x - y + log(-1 / (x + y))) / (x + y) << endl;
   }
   else {
-    pow(M_E, x - y);
+    cout << pow(M_E, x - y) << endl;
   }
   return 0;
 }
