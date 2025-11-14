7f394ffb0664ee70cc1c69b6088b7764e51a901b
حُب <159039354+cxurescry@users.noreply.github.com>
2025-11-15 02:37:41 +0700

    -

--- for uni /инф 1.2 (C++) /lab 5 /04.cpp
+++ for uni /инф 1.2 (C++) /lab 5 /04.cpp
@@ -2,7 +2,7 @@
 #include <iomanip>
 #include <string>
 
-std::string toBinary(unsigned int num) {
+std::string toBinary(unsigned short num) {
     std::string bin = "";
     for (int i = 15; i >= 0; --i) {
         bin += ((num >> i) & 1) ? '1' : '0';
@@ -16,12 +16,12 @@ int main() {
     std::cout << "Введите число ";
     std::cin >> X;
 
-    unsigned int X_unsigned = static_cast<unsigned int>(X);
-    unsigned int X_plus_1_unsigned = X_unsigned + 1;
+    unsigned short X_unsigned = static_cast<unsigned short>(X);
+    unsigned short X_plus_1_unsigned = X_unsigned + 1;
 
     std::cout << "       |  дес  |  восм  |  hex  |  двоичный" << std::endl;
-    std::cout << "x =    | " << std::setw(5) << X_unsigned << " | " << std::setw(6) << std::oct << X_unsigned << " | " << std::setw(4) << std::hex << X_unsigned << " | " << toBinary(X_unsigned) << std::endl;
-    std::cout << "x + 1 =| " << std::setw(5) << X_plus_1_unsigned << " | " << std::setw(6) << std::oct << X_plus_1_unsigned << " | " << std::setw(4) << std::hex << X_plus_1_unsigned << " | " << toBinary(X_plus_1_unsigned) << std::endl;
+    std::cout << "x =    | " << std::setw(5) << std::dec << X_unsigned << " | " << std::setw(6) << std::oct << X_unsigned << " | " << std::setw(4) << std::hex << X_unsigned << " | " << toBinary(X_unsigned) << std::endl;
+    std::cout << "x + 1 =| " << std::setw(5) << std::dec << X_plus_1_unsigned << " | " << std::setw(6) << std::oct << X_plus_1_unsigned << " | " << std::setw(4) << std::hex << X_plus_1_unsigned << " | " << toBinary(X_plus_1_unsigned) << std::endl;
 
     return 0;
 }
