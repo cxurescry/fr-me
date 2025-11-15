e1b0d2acd5b3f8fcd0fb646925feec5c4b0f79db
حُب <159039354+cxurescry@users.noreply.github.com>
2025-11-15 13:07:47 +0700

    -

--- for uni /инф 1.2 (C++) /lab 5 /06.cpp
+++ for uni /инф 1.2 (C++) /lab 5 /06.cpp
@@ -2,25 +2,29 @@
 #include <iomanip>
 #include <string>
 
+// Функция для вывода числа в двоичном виде (32 бита для int)
 std::string toBinary(unsigned int num) {
     std::string bin = "";
     for (int i = 15; i >= 0; --i) {
         bin += ((num >> i) & 1) ? '1' : '0';
-        if (i % 4 == 0 && i != 0) bin += ' ';
+        if (i % 4 == 0 && i != 0) bin += ' '; // Разделение по 4 бита для удобства
     }
     return bin;
 }
 
 int main() {
     signed int X;
-    std::cout << "Введите число ";
+    std::cout << "Введите целое десятичное число X (может быть отрицательным): ";
     std::cin >> X;
 
+    // Используем переменную с модификатором unsigned для форматирования вывода
     unsigned int X_unsigned = static_cast<unsigned int>(X);
 
-    std::cout << "Число хранится в компьютере в дополнительном коде (signed int)." << std::endl;
-    std::cout << "       |  дес  |  восм  |  hex  |  двоичный" << std::endl;
-    std::cout << "x =    | " << std::setw(5) << X_unsigned << " | " << std::setw(6) << std::oct << X_unsigned << " | " << std::setw(4) << std::hex << X_unsigned << " | " << toBinary(X_unsigned) << std::endl;
-
+    std::cout << "\nЧисло X в различных системах счисления (беззнаковое представление):" << std::endl;
+    std::cout << "       |   десятичная   |   восьмеричная   |   шестнадцатеричная   |   двоичная" << std::endl;
+    std::cout << "X =    | " << std::setw(14) << std::dec << X_unsigned << " | "
+              << std::setw(16) << std::oct << X_unsigned << " | "
+              << std::setw(21) << std::hex << std::uppercase << X_unsigned << " | "
+              << toBinary(X_unsigned) << std::endl;
     return 0;
 }
